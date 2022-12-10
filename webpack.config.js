32565c47ab0858e5917df2fbf8a5fdea5ece200c
NikitaBarashkov <nik96barashkov@yandex.by>
2022-12-10 11:24:12 +0300

    docs: create structure for the project (Sat, 10 December 2022, 11:23)

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'app', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+    new MiniCSSExtractPlugin({
+      filename: '[name].[contenthash].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg|jpeg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [MiniCSSExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }  
+    ]
+  }
+}
