1871df4f73e1ece85b21871a7012fa475fd401ea
NikitaBarashkov <nik96barashkov@yandex.by>
2022-12-15 14:24:05 +0300

    fix: change work module for filtration ticktes (Thu, 15 December 2022, 14:24)

--- src/app/countTransfers.js
+++ src/app/countTransfers.js
@@ -1,35 +1,25 @@
 function getOptions() {
   const options = document.querySelectorAll('.checkbox_hid');
-  
+
   return {
     allTickets: options[0].checked,
-    withoutTransfers: options[1].checked,
-    oneTransfer: options[2].checked,
-    twoTransfres: options[3].checked,
-    treeTransfers: options[4].checked
+    0: options[1].checked,
+    1: options[2].checked,
+    2: options[3].checked,
+    3: options[4].checked
   }
 }
 
 function filterTickets(array, object) {
   if (object.allTickets) return array;
-  const arr = [];
-  if (object.withoutTransfers) {
-    let selectedTicets = array.filter(elem => elem.segments[0].stops.length == 0 && elem.segments[1].stops.length == 0);
-    arr.push(selectedTicets);
-  }
-  if (object.oneTransfer) {
-    let selectedTicets = array.filter(elem => elem.segments[0].stops.length == 1 && elem.segments[1].stops.length == 1);
-    arr.push(selectedTicets);
-  }
-  if (object.twoTransfres) {
-    let selectedTicets = array.filter(elem => elem.segments[0].stops.length == 2 && elem.segments[1].stops.length == 2);
-    arr.push(selectedTicets);
-  }
-  if (object.treeTransfers) {
-    let selectedTicets = array.filter(elem => elem.segments[0].stops.length == 3 && elem.segments[1].stops.length == 3);
-    arr.push(selectedTicets);
-  }
-  return arr.flat();
+  
+  const transfers = Object.entries(object)
+    .filter(elem => elem[1] === true)
+    .flat()
+    .filter(arr => typeof arr == 'string')
+    .map(elem => +elem);
+  
+  return array.filter(ticket => transfers.includes(ticket.segments[0].stops.length) && transfers.includes(ticket.segments[1].stops.length));
 }
 
 export {getOptions, filterTickets};
